08c3427ce85c2d4e653c39dca8e4ea75cd563e9a
Thalys Wolf <thalyswolf01@gmail.com>
2021-11-27 15:32:37 -0300

    chore: dependency injection of configs

--- src/application/pre_processing.py
+++ src/application/pre_processing.py
@@ -1,10 +1,13 @@
+from src.common.contract.application.pre_processing_contract import PreProcessingConfigContract
 from src.common.entity.pre_processing_response import PreProcessingResponse
 from src.common.contract.application.database_contract import DatabaseContract
 
 
 class PreProcessingApplication():
 
-    def __init__(self, base_contract: DatabaseContract) -> None:
+    def __init__(self, pre_processing_config: PreProcessingConfigContract, 
+                base_contract: DatabaseContract) -> None:
+                
         self._base_contract = base_contract
 
     def execute(self) -> PreProcessingResponse:
