957a0e39ee3b5566701c8c747df01fbd25b15978
Thalys Wolf <thalyswolf01@gmail.com>
2021-11-27 11:24:47 -0300

    refactor: raise not implemented

--- src/common/entity/bayesian_learning.py
+++ src/common/entity/bayesian_learning.py
@@ -1,4 +1,5 @@
-from sklearn.naive_bayes import GaussianNB
+from src.helpers.errors.not_implemented_yet import NotImplementedYetException
+# from sklearn.naive_bayes import GaussianNB
 
 from src.common.contract.entity.learn import LearnContract
 
@@ -7,11 +8,13 @@ class BayesianLearn(LearnContract):
     gaussian_nb = None
 
     def __init__(self) -> None:
+        raise NotImplementedYetException
         self.gaussian_nb = GaussianNB()
 
     def training(self, x, y):
-        self.gaussian_nb.fit(x, y)
+        raise NotImplementedYetException
+        # self.gaussian_nb.fit(x, y)
 
     def predict(self):
-        pass
+        raise NotImplementedYetException
         # self.gaussian_nb.predict([[0,0,1,2], [2,0,0,0]])
