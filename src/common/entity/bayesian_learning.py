4b8a96bd936910834df556ff2f20769333ef5e74
Thalys Wolf <thalyswolf01@gmail.com>
2021-11-27 11:22:13 -0300

    refactor: add import external lib

--- src/common/entity/bayesian_learning.py
+++ src/common/entity/bayesian_learning.py
@@ -1,10 +1,17 @@
+from sklearn.naive_bayes import GaussianNB
+
 from src.common.contract.entity.learn import LearnContract
 
 
 class BayesianLearn(LearnContract):
-    
-    def training(self):
-        print('BayesianLearn! Training ...')
+    gaussian_nb = None
+
+    def __init__(self) -> None:
+        self.gaussian_nb = GaussianNB()
+
+    def training(self, x, y):
+        self.gaussian_nb.fit(x, y)
 
     def predict(self):
         pass
+        # self.gaussian_nb.predict([[0,0,1,2], [2,0,0,0]])
