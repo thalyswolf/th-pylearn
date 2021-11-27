e58172ae6ab52c5276e74c69b5a265e23d363eac
Thalys Wolf <thalyswolf01@gmail.com>
2021-11-27 11:01:54 -0300

    refactor: add first class at factory

--- src/factory/learn_factory.py
+++ src/factory/learn_factory.py
@@ -1,5 +1,7 @@
 from src.helpers.enum.learning_algorithms import LearningAlgorithms
+from src.common.entity import BayesianLearn
 
 def get_learn_contract(learning_algorithm: int):
+    
     if LearningAlgorithms.BAYESIAN_LEARNING == learning_algorithm:
-        pass
+        return BayesianLearn()
