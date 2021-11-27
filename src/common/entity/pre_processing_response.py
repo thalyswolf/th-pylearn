36d9b33a5b83e325775772bb673258b16a5aff4d
Thalys Wolf <thalyswolf01@gmail.com>
2021-11-27 17:15:11 -0300

    refactor: typings at response class

--- src/common/entity/pre_processing_response.py
+++ src/common/entity/pre_processing_response.py
@@ -2,5 +2,9 @@ from typing import List
 
 class PreProcessingResponse():
     X: List
+    Y: List[List]
     Y_training: List[List]
     Y_test: List[List]
+    lines_test: int
+    lines_training: int
+    lines_total: int
