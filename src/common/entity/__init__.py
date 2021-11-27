e64f659ca252ee251944039dd3f64895a5fb7199
Thalys Wolf <thalyswolf01@gmail.com>
2021-11-27 13:13:26 -0300

    chore: comment while was not implemented

--- src/common/entity/__init__.py
+++
@@ -1,1 +0,0 @@
-from src.common.entity.bayesian_learning import *
--- src/factory/learn_factory.py
+++ src/factory/learn_factory.py
@@ -1,10 +1,10 @@
-from src.helpers.errors.invalid_learning_algorithm import InvalidLearningAlgorithmException
-from src.helpers.enum.learning_algorithms import LearningAlgorithms
-from src.common.entity import BayesianLearn
+# from src.helpers.errors.invalid_learning_algorithm import InvalidLearningAlgorithmException
+# from src.helpers.enum.learning_algorithms import LearningAlgorithms
+# from src.common.entity import BayesianLearn
 
-def get_learn_contract(learning_algorithm: int):
+# def get_learn_contract(learning_algorithm: int):
 
-    if LearningAlgorithms.BAYESIAN_LEARNING == learning_algorithm:
-        return BayesianLearn()
+#     if LearningAlgorithms.BAYESIAN_LEARNING == learning_algorithm:
+#         return BayesianLearn()
 
-    raise InvalidLearningAlgorithmException()
+#     raise InvalidLearningAlgorithmException()
