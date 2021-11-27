79bbba93c3a6dfb4831d5856d3daad5bf4c97c7c
Thalys Wolf <thalyswolf01@gmail.com>
2021-11-27 17:14:46 -0300

    refactor: add more functions on config entity

--- src/common/entity/pre_processing_config.py
+++ src/common/entity/pre_processing_config.py
@@ -1,3 +1,4 @@
+from typing import List
 from src.helpers.enum.action_for_outliers_and_null_values import ActionForOutliersAndNullValues
 
 
@@ -12,14 +13,25 @@ class PreProcessingConfig():
     """ Action for handle null data """
     _action_for_null_values: int = ActionForOutliersAndNullValues.DEFAULT_VALUE
 
-    def get_percent_test(self):
+    """ Keeping same scale on data values """
+    _keep_same_scale: bool = True
+
+    _encoder_columns: List[int] = []
+
+    def get_percent_test(self) -> float:
         return self._percent_test
 
-    def get_action_for_outliers(self):
+    def get_action_for_outliers(self) -> int:
         return self._action_for_outliers
 
-    def get_action_for_null_values(self):
+    def get_action_for_null_values(self) -> int:
         return self._action_for_null_values
+
+    def get_keep_same_scale(self) -> bool:
+        return self._keep_same_scale
+
+    def get_encoder_columns(self) -> List[int]:
+        return self._encoder_columns
 
     def set_percent_test(self, percent_test: float):
         self._percent_test = percent_test
@@ -29,3 +41,12 @@ class PreProcessingConfig():
 
     def set_action_for_null_values(self, action_for_null_values: int):
         self._action_for_null_values = action_for_null_values
+
+    def set_keep_same_scale(self, keep_same_scale: bool):
+        self._keep_same_scale = keep_same_scale
+
+    def set_encoder_columns(self, columns: List[int]):
+        self._encoder_columns = columns
+
+    def add_encoder_columns(self, index: int):
+        self._encoder_columns.append(index)
