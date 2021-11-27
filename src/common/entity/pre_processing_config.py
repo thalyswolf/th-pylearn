160ca632bb5d3407982d3c43b05a56f239226039
Thalys Wolf <thalyswolf01@gmail.com>
2021-11-27 15:30:29 -0300

    refactor: adjusment for getter and setter methods

--- src/common/entity/pre_processing_config.py
+++ src/common/entity/pre_processing_config.py
@@ -4,10 +4,28 @@ from src.helpers.enum.action_for_outliers_and_null_values import ActionForOutlie
 class PreProcessingConfig():
 
     """ Percentage of data used for testing 0.00 - 1.00 """
-    PERCENT_TEST: float = 0.15
+    _percent_test: float = 0.15
 
     """ Action for handle outlier data """
-    ACTION_FOR_OUTLIERS: int = ActionForOutliersAndNullValues.DEFAULT_VALUE
+    _action_for_outliers: int = ActionForOutliersAndNullValues.DEFAULT_VALUE
 
     """ Action for handle null data """
-    ACTION_FOR_NULL_VALUES: int = ActionForOutliersAndNullValues.DEFAULT_VALUE
+    _action_for_null_values: int = ActionForOutliersAndNullValues.DEFAULT_VALUE
+
+    def get_percent_test(self):
+        return self._percent_test
+
+    def get_action_for_outliers(self):
+        return self._action_for_outliers
+
+    def get_action_for_null_values(self):
+        return self._action_for_null_values
+
+    def set_percent_test(self, percent_test: float):
+        self._percent_test = percent_test
+
+    def set_action_for_outliers(self, action_for_outliers: int):
+        self._action_for_outliers = action_for_outliers
+
+    def set_action_for_null_values(self, action_for_null_values: int):
+        self._action_for_null_values = action_for_null_values
