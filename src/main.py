be2c08686fd2c0513a2df301ef6aae6fddcb3125
Thalys Wolf <thalyswolf01@gmail.com>
2021-11-27 10:20:11 -0300

    chore: add main layer

--- src/main.py
+++ src/main.py
@@ -1,2 +1,21 @@
+from src.helpers.enum.learning_algorithms import LearningAlgorithms
+
+
 class ThPyLearn():
-    pass
+
+    learning_algorithm = LearningAlgorithms.BAYESIAN_LEARNING
+
+    def __init__(self, learning_algorithm: int):
+        self.learning_algorithm = learning_algorithm
+
+    def training():
+        """ NotImplementedYet """
+        pass
+
+    def predict():
+        """ NotImplementedYet """
+        pass
+
+    def generate_matrix_confustion():
+        """ NotImplementedYet """
+        pass
