6f58aa0fb72761e753e4e46b6a8ea5457d435f8d
Thalys Wolf <thalyswolf01@gmail.com>
2021-11-27 17:17:44 -0300

    refactor: adjusment at main layer

--- src/main.py
+++ src/main.py
@@ -1,3 +1,5 @@
+from src.common.contract.application.pre_processing_contract import PreProcessingConfigContract
+from src.common.entity.pre_processing_config import PreProcessingConfig
 from src.application.pre_processing import PreProcessingApplication
 from src.common.entity.pre_processing_response import PreProcessingResponse
 from src.helpers.enum.database_types import DatabaseTypes
@@ -10,9 +12,15 @@ class ThPyLearn():
         raise NotImplementedYetException
 
 class ThPreProcessing():
-    def __init__(self, database_type: int = DatabaseTypes.DEFAULT_DATABASE_TYPE):
-        self.database_type = database_type
-        self.database_contract = get_database_contract(self.database_type, {})
+    def __init__(self, 
+                pre_processing_config: PreProcessingConfigContract = PreProcessingConfig(), 
+                database_type: int = DatabaseTypes.DEFAULT_DATABASE_TYPE, 
+                database_data={}):
 
-    def pre_processing(self) -> PreProcessingResponse:
-        return PreProcessingApplication(self.database_contract).execute()
+        self.database_type = database_type
+        self.database_contract = get_database_contract(self.database_type, database_data)
+        self.pre_processing_config = pre_processing_config
+
+    def pre_processing(self, start_predictor_index: int, end_predictor_index: int, classifier_index: int) -> PreProcessingResponse:
+        return PreProcessingApplication(self.pre_processing_config, self.database_contract)\
+                                        .execute(start_predictor_index, end_predictor_index, classifier_index)
