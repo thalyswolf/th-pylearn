5e58f79b3c3ac42d61115bcafdb7475a36d46cee
Thalys Wolf <thalyswolf01@gmail.com>
2021-11-27 17:16:41 -0300

    refactor: read csv implements

--- src/infra/databases/csv.py
+++ src/infra/databases/csv.py
@@ -1,16 +1,21 @@
+from typing import List
+import pandas as pd
+
 from src.common.contract.application.database_contract import DatabaseContract
 
 
 class CSV(DatabaseContract):
 
     def __init__(self, data) -> None:
-        self.data = data
+        self.data = pd.read_csv(data)
+        print(self.data.head())
 
-    def extract_X(self) -> None:
-        return [0,1,2,3]
 
-    def extract_Y(self) -> None:
-        return [[0,1,2,3], [0,1,2,3], [0,1,2,3]]
+    def extract_X(self, start_predictor_index: int, end_predictor_index: int) -> List[List]:
+        return self.data.iloc[:, start_predictor_index:end_predictor_index].values
+
+    def extract_Y(self, classifier_index:int) -> List:
+        return self.data.iloc[:, classifier_index].values
 
     def separate_base_learning_and_test(self, percent: int) -> None:
         pass
